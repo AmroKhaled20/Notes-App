ed4be9c3254dd6c4529bc0e61050fe06462fe278
Amrokhaled20 <kamr82460@gmail.com>
2026-01-16 19:52:49 +0200

    Create custom note list and add floating action button in notes view

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_bar_widget.dart';
 import 'package:notes_app/widgets/custom_notecard_widget.dart';
+import 'package:notes_app/widgets/custom_noteslist_widget.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: const Scaffold(
+      child: Scaffold(
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             children: [
               CustomBarWidget(),
-              SizedBox(height: 10,),
-              CustomNotecardWidget(),
-              ]),
+              SizedBox(height: 10),
+              Expanded(child: CustomNoteslistWidget()),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+
+          backgroundColor: const Color.fromARGB(255, 73, 143, 249),
+          child: IconButton(onPressed: () {}, icon: Icon(Icons.add, size: 35)),
         ),
       ),
     );
