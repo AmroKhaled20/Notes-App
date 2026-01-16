140996c462c920356fb4053f461b7f56630cc95d
Amrokhaled20 <kamr82460@gmail.com>
2026-01-16 19:02:05 +0200

    Create custom widgets in notes view

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_bar_widget.dart';
+import 'package:notes_app/widgets/custom_notecard_widget.dart';
 
-class NotesView extends StatelessWidget{
+class NotesView extends StatelessWidget {
   const NotesView();
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: const Scaffold(
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              CustomBarWidget(),
+              SizedBox(height: 10,),
+              CustomNotecardWidget(),
+              ]),
+        ),
+      ),
+    );
   }
-
 }
