43c0ebf718d29d0f64418f70f841fba0340e3bc0
AmroKhaled20 <amrkhalidmain@gmail.com>
2026-01-25 01:34:55 +0200

    Create show modal bottom sheet design

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_addnote_bottomsheet_widget.dart';
 import 'package:notes_app/widgets/custom_bar_widget.dart';
 import 'package:notes_app/widgets/custom_notecard_widget.dart';
 import 'package:notes_app/widgets/custom_noteslist_widget.dart';
@@ -20,10 +21,21 @@ class NotesView extends StatelessWidget {
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadiusGeometry.circular(32),
+              ),
+              context: context,
+              builder: (context) {
+                return CustomAddnoteBottomsheetWidget();
+              },
+            );
+          },
 
           backgroundColor: const Color.fromARGB(255, 73, 143, 249),
-          child: IconButton(onPressed: () {}, icon: Icon(Icons.add, size: 35)),
+          child: Icon(Icons.add, size: 35),
         ),
       ),
     );
